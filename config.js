cadf370c98b1b561c836103188d30d523e0dc165
sagarchoru6 <sagar.choru38@gmail.com>
2018-10-29 23:30:40 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '8KUk6UoAumbRNFflOcgBBxjMO',
+    consumer_secret: 'khiUzclXat2wJt5lBH2Fnv6znfuy12kXuX4JNrXVvT00asX1oI',
+    access_token_key: '954012626-EpsF9c6CPtUKQRlbch9aoN6u7jikAFWYXlTjlOom',
+    access_token_secret: 'r69qgjuVB2xDfUIkJLNoHHjqGfImn4VonD52jvA3SqcyZ'
+}
